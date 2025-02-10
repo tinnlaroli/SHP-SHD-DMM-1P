cc4c3cf7a3afa54642ca49380618c0968ebc58ec
Martin <olivarezmartin714@gmail.com>
2025-02-10 15:35:38 -0600

    build: tab ediciones

---
+++ src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsComponent } from './tabs.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsComponent,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('../pages/home/home.module').then(m => m.HomePageModule)
+      },
+      {
+        path: 'discover',
+        loadChildren: () => import('../pages/discover/discover.module').then(m => m.DiscoverPageModule)
+      },
+      {
+        path: 'chat',
+        loadChildren: () => import('../pages/chat/chat.module').then(m => m.ChatPageModule)
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('../pages/profile/profile.module').then(m => m.ProfilePageModule)
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsRoutingModule {}
---
+++ src/app/tabs/tabs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'app-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.scss'],
+})
+export class TabsComponent {
+  constructor() {}
+}
